1e6b69a9e309eb36e030689816b3ebe644afc7bd
Thiago Felipe <tfofelipeoliveira19@hotmail.com>
2025-08-19 15:47:22 -0300

    função adicionarAmigo adicionada

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 let listaDeNomes = [];
+
+function adicionarAmigo() {
+    let nomeRecebido = document.getElementById("amigo").value;
+
+    if (nomeRecebido == "") {
+        alert("Por favor, insira um nome válido");
+    } else {
+        listaDeNomes.push(nomeRecebido);
+        atualizarLista();
+        document.getElementById("amigo").value = "";
+    }
+}
